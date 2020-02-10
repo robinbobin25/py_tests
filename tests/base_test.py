34857fe973c0c1b82a316fccfb3994b2b25f25d5
tonyamelnychuk <antonina.mykhailenko@gmail.com>
2020-02-10 15:16:46 +0200

    add

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,10 +1,22 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import allure
+from allure_commons.types import AttachmentType
 
-#create a new Chrome session
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.maximize_window()
-driver.get("https://www.google.com")
+class TestGoogleSearch:
 
-#clovz
-driver.quit()
+    def setup(self):
+        #create a new Chrome session
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.maximize_window()
+
+    @allure.feature('Open pages')
+    @allure.story('Open Google')
+    def test_google_search(self):
+        self.driver.get("https://www.google.com")
+        with allure.step("Screenshot shoot"):
+            allure.attach(self.driver.get_screenshot_as_png(), name="pic", attachment_type=AttachmentType.PNG)
+        assert self.driver.title == 'Google'
+
+    def teardown(self):
+        self.driver.quit()
